5a419106de19581f3f54062bf5e041f4f89722ce
Jorge Alejandro Ramos Gonzalez <alexramosglz@MacBook-Pro-de-Jorge.local>
2025-01-14 21:24:32 -0600

    footer style finished

--- src/components/footer/Footer.component.jsx
+++ src/components/footer/Footer.component.jsx
@@ -3,6 +3,7 @@ export default function Footer() {
     <footer className="footer">
       <ul className="sections">
         <li className="section social">
+          <h3>Follow Me</h3>
           <a href="https://github.com/AlexRamosGlz" target="_blank">
             <img
               src="https://img.icons8.com/?size=100&id=106562&format=png&color=e2e2e2"
@@ -22,44 +23,49 @@ export default function Footer() {
           </a>
         </li>
         <li className="section legal">
+          <h3>Legal</h3>
           <ul className="section-options">
             <li>
-              <a>Lorem ipsum dolor</a>
+              <a>Privacy</a>
             </li>
             <li>
-              <a>consectetur adipiscing</a>
-            </li>
-            <li>
-              <a>sed do eiusmod tempor</a>
-            </li>
-            <li>
-              <a>dolore magna aliqua</a>
-            </li>
-            <li>
-              <a>Ut enim ad minim aqua</a>
+              <a>Cookies</a>
             </li>
           </ul>
         </li>
         <li className="section Privacy">
+          <h3>Privacy</h3>
           <ul className="section-options">
             <li>
-              <a>Lorem ipsum dolor</a>
+              <a>Conditions</a>
             </li>
             <li>
-              <a>consectetur adipiscing</a>
+              <a>Permitions</a>
             </li>
             <li>
-              <a>sed do eiusmod tempor</a>
+              <a>Contract</a>
             </li>
             <li>
-              <a>dolore magna aliqua</a>
+              <a>Provacy Policy</a>
+            </li>
+          </ul>
+        </li>
+        <li className="section company">
+          <h3>Company</h3>
+          <ul className="section-options">
+            <li>
+              <a>FAQ</a>
             </li>
             <li>
-              <a>Ut enim ad minim aqua</a>
+              <a>About us</a>
+            </li>
+            <li>
+              <a>Work with us</a>
             </li>
           </ul>
         </li>
       </ul>
+      <p className="copyright">&copy; Copyright 2024. All right reserved</p>
     </footer>
   );
 }
